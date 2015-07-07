25f09633f7427ed3e10c7ee23fdb8321f94fdc45
pericles <pericles3d@gmail.com>
2015-07-07 00:20:08 -0700

    Better script for bombs generation. Adjusted id names

--- js/script.js
+++ js/script.js
@@ -9,11 +9,27 @@ for (var i = 0; i < bombAmount; i++){
   bombGenerate[i] = [
     Math.ceil(Math.random() * rows),
     Math.ceil(Math.random() * columns)];
-}
+    if(bombGenerate[i][0].toString().length === 1 && bombGenerate[i][1].toString().length === 1){
+      document.querySelector('#_0'+bombGenerate[i][0]+'_0'+bombGenerate[i][1]).innerText = "B";
+    } else if (bombGenerate[i][0].toString().length === 1 && bombGenerate[i][1].toString().length === 2){
+      document.querySelector('#_0'+bombGenerate[i][0]+'_'+bombGenerate[i][1]).innerText = "B";
+    } else if (bombGenerate[i][0].toString().length === 2 && bombGenerate[i][1].toString().length === 1){
+      document.querySelector('#_'+bombGenerate[i][0]+'_0'+bombGenerate[i][1]).innerText = "B";
+    } else {
+      document.querySelector('#_'+bombGenerate[i][0]+'_'+bombGenerate[i][1]).innerText = "B";
+    }
+  }
 
-for (var i = 0; i < bombAmount; i++){
-  document.querySelector('#_'+bombGenerate[i][0]+'_'+bombGenerate[i][1]).innerText = "B";
-}
+
+//Generating number of bombs surrounding each box.
+
+// var eachBox = [];
+//
+// for (var i = 0; i < (rows*columns); i++){
+//   eachBox[i] = [
+//
+//   ]
+//   }
 
 
 
